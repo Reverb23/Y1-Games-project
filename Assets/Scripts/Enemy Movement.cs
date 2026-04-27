2b52139de76511fefb857d0e894d21e78a8fa312
Jamie W <163033606+Reverb23@users.noreply.github.com>
2026-04-27 18:04:36 +0100

    Enemy progress

--- Assets/Scripts/Enemy Movement.cs
+++ Assets/Scripts/Enemy Movement.cs
@@ -1,19 +1,28 @@
 using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
-    private int damage = 5
-    private float speed = 1.5f
-    private float health = 50.0f
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private int Damage = 5;
+    private float Speed = 1.5f;
+    private float Health = 50.0f;
+    private EnemyStats data;
+
+
     void Start()
     {
-        
+        Player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-     
+        Chase();
+
+    }
+    private void Chase()
+    {
+
+
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, Speed*Time.deltaTime);
     }
 }
