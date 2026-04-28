4e5ea2e341b99488ab3df575bd578af147608735
Jamie W <163033606+Reverb23@users.noreply.github.com>
2026-04-28 14:19:00 +0100

    RAAAAAAAAAHHHHHHHHHHH GIT ISSUE FIXED

--- Assets/Scripts/Enemy Movement.cs
+++ Assets/Scripts/Enemy Movement.cs
@@ -1,10 +1,14 @@
+using System;
 using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
-    private int damage = 5
-    private float speed = 1.5f
-    private float health = 50.0f
 {
+    private int Damage = 5;
+    private float Speed = 1.5f;
+    private float Health = 50.0f;
+    Vector3 PlayerPos = GameObject.FindWithTag("Player").transform.position;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,6 +18,14 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-     
+        MoveToPlayer();
+
+    }
+
+    void MoveToPlayer()
+    {
+        
+        transform.position = transform.position + (PlayerPos*Speed*Time.deltaTime);
+        Debug.Log(PlayerPos);
     }
 }
