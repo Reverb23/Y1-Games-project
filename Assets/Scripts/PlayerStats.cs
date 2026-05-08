55c0f920c5a44f1c7a492e75839ad517c52fc8cc
Jamie W <163033606+Reverb23@users.noreply.github.com>
2026-05-08 12:27:52 +0100

    forgor

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -10,6 +10,7 @@ public class PlayerStats : MonoBehaviour
     {  
         get { return _PlayerLevel; }
     }
+    
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
